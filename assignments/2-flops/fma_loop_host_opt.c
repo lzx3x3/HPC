1951ada3d98cdc647d53494bdc8a51b90f11168b
Muyang Guo <mguo34@coc-ice.pace.gatech.edu>
2019-09-05 00:27:26 -0400

    finished assignment2

--- assignments/2-flops/fma_loop_host_opt.c
+++ assignments/2-flops/fma_loop_host_opt.c
@@ -18,9 +18,20 @@
 void
 fma_loop_host (int N, int T, float *a, float b, float c)
 {
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < T; j++) {
+  #pragma unroll(12)
+  for (int i = 0; i < N; i+=8) {
       a[i] = a[i] * b + c;
-    }
+      a[i+1] = a[i+1]*b + c;
+      a[i+2] = a[i+2]*b + c;
+      a[i+3] = a[i+3]*b + c;
+      a[i+4] = a[i+4] * b + c;
+      a[i+5] = a[i+5]*b + c;
+      a[i+6] = a[i+6]*b + c;
+      a[i+7] = a[i+7]*b + c;
+
+
+
+
+    
   }
 }
