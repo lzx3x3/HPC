2e200211dcf0748f9ad937c18bc17cf614d1c875
Toby Isaac <tisaac@cc.gatech.edu>
2019-10-08 21:27:27 -0400

    Import dense matrix vector product example

---
+++ notes/mpi/dmv/dmv_args.c
@@ -0,0 +1,122 @@
+#include <string.h>
+#include <getopt.h>
+#include "dmv.h"
+#include "dmv_impl.h"
+
+static int OptionsGetEnum(const dmv_option *options, int numOptions, const char *optstring, int *optionChoice)
+{
+  int i;
+
+  for (i = 0; i < numOptions; i++) {
+    if (!strcmp(options[i].optname, optstring)) {
+      break;
+    }
+  }
+  *optionChoice = i;
+
+  return 0;
+}
+
+int ArgsCreate(MPI_Comm comm, int argc, char **argv, Args *args_p)
+{
+  Args     args = NULL;
+  int      err, opt, size;
+
+  args = (Args) calloc(1,sizeof(*args));
+  if (!args) MPI_CHK(1);
+
+  args->comm = comm;
+  args->scale = 20;
+  while ((opt = getopt(argc, argv, "s:e:g:l:v:d:p:m:")) != -1) {
+    switch (opt) {
+    case 's':
+      args->scale = atoi(optarg);
+      break;
+    case 'e':
+      args->seed.v[0] = atoi(optarg);
+      break;
+    case 'g':
+      err = OptionsGetEnum(GlobalSizeTypes, GLOBAL_SIZE_NUM_TYPES, optarg, &args->global_size_strategy); MPI_CHK(err);
+      break;
+    case 'l':
+      err = OptionsGetEnum(LayoutTypes, LAYOUT_NUM_TYPES, optarg, &args->layout_strategy); MPI_CHK(err);
+      break;
+    case 'v':
+      args->verbosity = atoi(optarg);
+      break;
+    case 'd':
+      args->debug = atoi(optarg);
+      break;
+    case 'p':
+      err = OptionsGetEnum(PartitionTypes, PARTITION_NUM_TYPES, optarg, &args->partition_strategy); MPI_CHK(err);
+      break;
+    case 'm':
+      err = OptionsGetEnum(MatVecTypes, MATVEC_NUM_TYPES, optarg, &args->matvec_strategy); MPI_CHK(err);
+      break;
+    default:
+      fprintf(stderr, "Usage: %s [-s scale] [-e seed] [-v verbosity] [-d debug]\n", argv[0]);
+      fprintf(stderr, "          [-g global_size_strategy] [-l layout_strategy]\n");
+      fprintf(stderr, "          [-p matrix_partition_strategy] [-m matvec_strategy]\n\n");
+      fprintf(stderr, "global_size_strategy:");
+      for (int i = 0; i < GLOBAL_SIZE_NUM_TYPES; i++) {
+        if (i) {fprintf(stderr, " , ");}
+        fprintf(stderr, ", %s", GlobalSizeTypes[i].optname);
+      }
+      fprintf(stderr, "\nlayout_strategy: ");
+      for (int i = 0; i < LAYOUT_NUM_TYPES; i++) {
+        if (i) {fprintf(stderr, " , ");}
+        fprintf(stderr, ", %s", LayoutTypes[i].optname);
+      }
+      fprintf(stderr, "\nmatrix_partition_strategy: ");
+      for (int i = 0; i < PARTITION_NUM_TYPES; i++) {
+        if (i) {fprintf(stderr, " , ");}
+        fprintf(stderr, ", %s", PartitionTypes[i].optname);
+      }
+      fprintf(stderr, "\nmatvec_strategy: ");
+      for (int i = 0; i < MATVEC_NUM_TYPES; i++) {
+        if (i) {fprintf(stderr, " , ");}
+        fprintf(stderr, ", %s", MatVecTypes[i].optname);
+      }
+      break;
+    }
+  }
+
+  err = MPI_Comm_size(comm, &size); MPI_CHK(err);
+
+  if (args->verbosity) {
+    MPI_LOG(comm,
+            "Distributed dense matrix-vector product example:\n"
+            "    Procs: %d\n    Scale: %d\n    Seed: %lu\n"
+            "    Global size: %s\n    Layout: %s\n    Partition: %s\n    Matvec: %s\n",
+            size, args->scale, args->seed.v[0],
+            GlobalSizeTypes[args->global_size_strategy].optname,
+            LayoutTypes[args->layout_strategy].optname,
+            PartitionTypes[args->partition_strategy].optname,
+            MatVecTypes[args->matvec_strategy].optname);
+  }
+
+  args->key = threefry4x64keyinit(args->seed);
+
+  *args_p = args;
+
+  return 0;
+}
+
+int ArgsDestroy(Args *args_p)
+{
+  free(*args_p);
+  *args_p = NULL;
+  return 0;
+}
+
+int ArgsGetMatVecType(Args args, int *type_p)
+{
+  *type_p = args->matvec_strategy;
+  return 0;
+}
+
+int ArgsGetPartitionType(Args args, int *type_p)
+{
+  *type_p = args->partition_strategy;
+  return 0;
+}
---
+++ notes/mpi/dmv/dmv_matvec.c
@@ -0,0 +1,25 @@
+
+#include "dmv.h"
+#include "dmv_impl.h"
+
+
+int DenseMatVec(Args args, int mStart, int mEnd, int nStart, int nEnd, const double *matrixEntries, int nLocal, const double *vecRightLocal, int mLocal, double *vecLeftLocal)
+{
+  int err;
+
+  for (int i = 0; i < mLocal; i++) {vecLeftLocal[i] = 0.;}
+  switch (args->partition_strategy) {
+  case PARTITION_ROWS:
+    if (args->verbosity) {MPI_LOG(args->comm, "DenseMatVec row partition\n");}
+    err = DenseMatVec_RowPartition(args, mStart, mEnd, nStart, nEnd, matrixEntries, nLocal, vecRightLocal, mLocal, vecLeftLocal); MPI_CHK(err);
+    break;
+  case PARTITION_COLS:
+    if (args->verbosity) {MPI_LOG(args->comm, "DenseMatVec column partition\n");}
+    err = DenseMatVec_ColPartition(args, mStart, mEnd, nStart, nEnd, matrixEntries, nLocal, vecRightLocal, mLocal, vecLeftLocal); MPI_CHK(err);
+    break;
+  default:
+    MPI_LOG(args->comm, "Unknown partition type %d\n", args->partition_strategy);
+    MPI_CHK(1);
+  }
+  return 0;
+}
